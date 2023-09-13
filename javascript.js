8ced2a59db364f0caa60313fba69601f7858ec44
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2023-09-12 23:55:22 -0700

    gridButtonChanger

--- javascript.js
+++ javascript.js
@@ -1,18 +1,37 @@
 const container = document.querySelector('body');
+const colorPicker = document.querySelector('input[type="color"]')
 
 const div = document.createElement('div')
 div.classList.add("container")
 container.appendChild(div);
 
-for (let i = 0; i < 16 * 16; i++) {
-    const squaredDiv = document.createElement('div')
-    squaredDiv.classList.add("squaredDiv")
-    div.appendChild(squaredDiv);
 
-    squaredDiv.addEventListener("mouseover", (event) => {
-        event.target.style.backgroundColor = colorPicker.value;
+function grid(rows, cols) {
+
+    div.innerText = ""
+    div.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    div.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+
+    for (let i = 0; i < rows * cols; i++) {
+        const squaredDiv = document.createElement('div')
+        squaredDiv.classList.add("squaredDiv")
+        div.appendChild(squaredDiv);
+
+        squaredDiv.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = colorPicker.value;
+        }
+        )
+
+
     }
-    )
 }
 
-const colorPicker = document.querySelector('input[type="color"]')
+document.addEventListener("DOMContentLoaded", function () {
+    grid(16, 16);
+});
+
+let gridSizeButton = document.querySelector(".gridButton");
+gridSizeButton.addEventListener("click", function () {
+    let size = prompt("Select your Grid Size")
+    grid(size, size)
+})
